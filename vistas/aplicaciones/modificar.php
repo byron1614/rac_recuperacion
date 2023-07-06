5e586c3c8b8c27fa65add8f54f2185b077a82f9a
byron <gemechama11203jorge@gmail.com>
2023-07-06 13:45:23 -0600

    se realizaron modifcaciones

--- vistas/aplicaciones/modificar.php
+++ vistas/aplicaciones/modificar.php
@@ -1,21 +1,36 @@
+<?php
+require '../../modelos/Aplicacion.php';
+    try {
+      
+        $aplicacion = new Aplicacion($_GET);
+        
+        $app = $aplicacion->buscar();
+        //var_dump($app);
+    } catch (PDOException $e) {
+        $error = $e->getMessage();
+    } catch (Exception $e2){
+        $error = $e2->getMessage();
+    }
+?>
 <?php include_once '../../includes/header.php'?>
-<?php include_once '../../includes/navbar.php'?>
-<div class="container mt-5">
-  <h1 class="text-center mt-3">Formulario para la creación de Aplicaciones</h1>
-  <div class="row justify-content-center mt-2">
-    <form action="/rac_recuperacion/controladores/aplicacion/guardar.php" method="POST" class="col-lg-8 border bg-light p-3">
-      <div class="row mb-3">
-        <div class="col">
-          <label for="apli" class="form-label">Ingrese el nombre de la aplicación</label>
-          <input type="text" name="apli_nombre" id="apli_nombre" class="form-control" required>
+    <div class="container">
+        <h1 class="text-center">Modificar aplicacion</h1>
+        <div class="row justify-content-center">
+            <form action="/rac_recuperacion/controladores/aplicacion/modificar.php" method="POST" class="col-lg-8 border bg-light p-3">
+                <input type="hidden" name="apli_id" value="<?= $app[0]['APLI_ID'] ?>" >
+                <div class="row mb-3">
+                    <div class="col">
+                        <label for="gra_nombre">Nombre de la aplicacion</label>
+                        <input type="text" name="apli_nombre" id="apli_nombre" value="<?= $app[0]['APLI_NOMBRE'] ?>" class="form-control" required>
+                    </div>
+                </div>
+                
+                <div class="row mb-3">
+                    <div class="col">
+                        <button type="submit" class="btn btn-warning w-100">Modificar</button>
+                    </div>
+                </div>
+            </form>
         </div>
-      </div>
-      <div class="row mb-3">
-        <div class="col">
-          <button type="submit" class="btn btn-primary w-100">Guardar</button>
-        </div>
-      </div>
-    </form>
-  </div>
-</div>
+    </div>
 <?php include_once '../../includes/footer.php'?>
